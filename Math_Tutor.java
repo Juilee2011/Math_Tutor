810f1f5773eccb1d049b514543b26ca53548981b
Juilee Joshi <2149185@jeffcoschools.us>
2026-09-15 22:56:02 -0600

    Added Math_Tutor class

--- Math_Tutor.java
+++ Math_Tutor.java
@@ -1,3 +1,18 @@
-public class Math_Tutor {
-    
-}
+ /*
+ * Math_Tutor
+ * Juilee Joshi
+ * Helps solve 6 types of problems
+ *
+ * Resources :
+ *
+ */
+
+ import java . util . Scanner ;
+
+ public class MathTutor
+ {
+ public static void main ( String [] args ) throws IOException
+ {
+
+ }
+ }
